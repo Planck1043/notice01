3526dbe8cb122613793bba7dcc4e41670d5de1f2
Planck1043 <zhao1036713209@gmail.com>
2018-02-09 22:41:41 +0800

    debug01

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :sender, class_name: "Letter", foreign_key: "send_id"
+  has_many :recer, class_name: "Letter", foreign_key: "rec_id"
 end
--- db/migrate/20180208100802_add_send_id_to_letter.rb
+++ db/migrate/20180208100802_add_send_id_to_letter.rb
@@ -1,5 +1,7 @@
 class AddSendIdToLetter < ActiveRecord::Migration[5.1]
   def change
     add_column :letters, :send_id, :integer
+    add_reference :letters, :sender
+    add_reference :letters, :recever
   end
 end
