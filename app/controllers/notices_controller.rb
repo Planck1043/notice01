2a201473e5f018312834ab17721d3671ddc3cf48
Planck1043 <zhao1036713209@gmail.com>
2018-02-23 15:48:55 +0800

    install notice will_paginate

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -1,6 +1,6 @@
 class NoticesController < ApplicationController
   def index
-    @notices = Notice.all
+    @notices = Notice.all.paginate(:page => params[:page], :per_page => 10)
   end
 
   def new
